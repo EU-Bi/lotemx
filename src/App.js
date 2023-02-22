53fe1d4ac264d7a2a52d79b7effd53ab6c025273
EU-Bi <en_bilan@icloud.com>
2023-02-22 23:27:23 +0200

    testtask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Users } from './users';
 
 function App() {
+  const [query, setQuery]=useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapp-top'>
+        <div className='textUsers'>
+          <h3>Users</h3>
+          <p>{Users.length}</p>
+        </div>
+        <div className="search">
+          <input type="text" className="search-field" onChange={(e)=>setQuery((e.target.value).toLowerCase())} placeholder="Search"/>
+          <div className="search-icon"/>
+        </div>
+      </div>
+      <div className='wrapp-center'>
+        <div className='rowHeadList'>
+          <div className='right'>
+            <p>User Name</p>
+            <div />
+          </div>
+          <div className='left'>
+            <p>View</p>
+            <p>Execute</p>
+            <p>Modify</p>
+            <p>Delete</p>
+          </div>
+        </div>
+        <ul>
+          {Users.filter((user)=>user.name.toLowerCase().includes(query)).map(e=>{
+            return(
+              <li>
+                <div>
+                  <span></span>
+                  <p>{e.name}</p>
+                </div>
+                <div className='checkboxs'>
+                  <input type="checkbox"/>
+                  <input type="checkbox"/>
+                  <input type="checkbox"/>
+                  <input type="checkbox"/>
+                  <div className='dots'></div>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <button>Save</button>
     </div>
   );
 }
---
+++ src/users.js
@@ -0,0 +1,92 @@
+export const Users = [
+  {
+    name:"Username 1"
+  },
+  {
+    name:"Username 2"
+  },
+  {
+    name:"Username 3"
+  },
+  {
+    name:"Username 4"
+  },
+  {
+    name:"Username 5"
+  },
+  {
+    name:"Username 6"
+  },
+  {
+    name:"Username 7"
+  },
+  {
+    name:"Username 8"
+  },
+  {
+    name:"Username 9"
+  },
+  {
+    name:"Username 10"
+  },
+  {
+    name:"Username 11"
+  },
+  {
+    name:"Username 12"
+  },
+  {
+    name:"Username 13"
+  },
+  {
+    name:"Username 14"
+  },
+  {
+    name:"Username 15"
+  },
+  {
+    name:"Username 16"
+  },
+  {
+    name:"Username 17"
+  },
+  {
+    name:"Username 18"
+  },
+  {
+    name:"Username 19"
+  },
+  {
+    name:"Username 20"
+  },
+  {
+    name:"Username 21"
+  },
+  {
+    name:"Username 22"
+  },
+  {
+    name:"Username 23"
+  },
+  {
+    name:"Username 24"
+  },
+  {
+    name:"Username 25"
+  },
+  {
+    name:"Username 26"
+  },
+  {
+    name:"Username 27"
+  },
+  {
+    name:"Username 28"
+  },
+  {
+    name:"Username 29"
+  },
+  {
+    name:"Username 30"
+  },
+]
